3dd06bb22cbf982b8f78b266a095e0e60d5323c4
johannes walcher <johannes.walcher@stusta.de>
2017-04-21 15:06:23 +0200

    Added even more lists. Unfortunately they will start breaking the ASTA system

--- registration/templatetags/latex.py
+++ registration/templatetags/latex.py
@@ -1,3 +1,5 @@
+from django.db.models.functions import Lower
+from django.db.models.query import QuerySet
 from django.template import Library
 from django.template.defaultfilters import stringfilter
 from latex import escape
@@ -20,3 +22,7 @@ def bykey(value, arg):
     except:
         return ''
 
+@register.filter
+def helpersort(value, arg):
+    return value.extra(select={'case_insensitive_order': 'LOWER(' + arg + ')'})\
+        .order_by('case_insensitive_order')
